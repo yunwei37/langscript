3c9b7d60b103103a817252f6a30577d6467a2e49
云微 <1067852565@qq.com>
2023-05-14 06:28:14 +0000

    update the document of concept

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import io
 import os
 from setuptools import find_packages, setup
 
-
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
     >>> read("langscript", "VERSION")
@@ -20,14 +19,12 @@ def read(*paths, **kwargs):
         content = open_file.read().strip()
     return content
 
-
 def read_requirements(path):
     return [
         line.strip()
         for line in read(path).split("\n")
         if not line.startswith(('"', "#", "-", "git+"))
     ]
-
 
 setup(
     name="langscript",
